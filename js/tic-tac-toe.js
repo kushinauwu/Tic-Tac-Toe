cf6bb24af5f963e0fa96d7de63234ca8d289dd6f
Deepashree Deshmukh <18piscean@gmail.com>
2019-02-05 16:41:13 +0530

    it works!

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -21,48 +21,50 @@ function nextMove(square) {
 
 //switch turns of players
 function switchTurn() {
-    if(checkForWinner(document.turn)) {
+    if(checkWinner(document.turn)) {    //if checkWInner returns true
         setMessage(document.turn + " wins!");
-    }
-    else if (document.turn == 'X') {
+    } else if (document.turn == 'X') {
         document.turn = 'O';
-            setMessage(document.turn + " plays now!");
+                setMessage(document.turn + " plays now!");
+
 
     } else {
         document.turn = 'X';
-            setMessage(document.turn + " plays now!");
+        setMessage(document.turn + " plays now!");
 
     }
+
 }
 
-//check for winner
-function checkForWinner(move) {
+function checkWinner(move) {
     var result = false;
-    if (checkRow(0, 1, 2, move) ||
-        checkRow(3, 4, 5, move) ||
-        checkRow(6, 7, 8, move) ||
-        checkRow(0, 3, 6, move) ||
-        checkRow(1, 4, 7, move) ||
-        checkRow(2, 5, 8, move) ||
-        checkRow(0, 4, 8, move) ||
-        checkRow(2, 4, 6, move)) {  //winning combinations are across, sideways and diagonally
-        result = true;
+    if(checkRow(1, 2, 3, move) || checkRow(4, 5, 6, move) || checkRow(7, 8, 9, move) || checkRow(1, 4, 7, move) || checkRow(2, 5, 8, move) || checkRow(3, 6, 9, move) || checkRow(1, 5, 9, move) || checkRow(3, 5, 7, move)) {
+        var result = true;  //true if the 3 letters are the same across, sideways and diagonally
+
     }
     return result;
 }
 
-//check row of 3 
+//check for rows with same letters
 function checkRow(a, b, c, move) {
     var result = false;
     if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
-        result = true;  //if a, b, c have the same letter, return true
+        result = true;  //check if a, b, c have same value
+                makeGreen(a,b,c);   
+
     }
     return result;
 }
 
-//get the box id 
+//get id of the box the letter is in
 function getBox(number) {
-    document.getElementById("s" + number).innerHTML;
+   return document.getElementById("s" + number).innerHTML;
+}
+
+function makeGreen(a, b, c) {
+    document.getElementById("s" + a).style.backgroundColor = "lightgreen";
+    document.getElementById("s" + b).style.backgroundColor = "lightgreen";
+    document.getElementById("s" + c).style.backgroundColor = "lightgreen";
 }
 
 
