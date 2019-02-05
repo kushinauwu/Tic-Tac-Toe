f49932dbb0e5d17d9c45867c5a0e62f969f60f1d
Deepashree Deshmukh <18piscean@gmail.com>
2019-02-05 14:06:18 +0530

    square already in use message

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -11,17 +11,78 @@ function setMessage(msg) {
 
 //make the next move
 function nextMove(square) {
-    square.innerHTML = document.turn;
-    switchTurn();
+    if (square.innerHTML == "") {
+        square.innerHTML = document.turn;
+        switchTurn();
+    } else {
+        setMessage("This square is already used!");
+    }
 }
 
 //switch turns of players
 function switchTurn() {
-    if (document.turn == 'X') {
+    if(checkForWinner(document.turn)) {
+        setMessage(document.turn + " wins!");
+    }
+    else if (document.turn == 'X') {
         document.turn = 'O';
-    }
-    else {
+            setMessage(document.turn + " plays now!");
+
+    } else {
         document.turn = 'X';
+            setMessage(document.turn + " plays now!");
+
     }
-    setMessage(document.turn + " plays now!");
 }
+
+//check for winner
+function checkForWinner(move) {
+    var result = false;
+    if (checkRow(0, 1, 2, move) ||
+        checkRow(3, 4, 5, move) ||
+        checkRow(6, 7, 8, move) ||
+        checkRow(0, 3, 6, move) ||
+        checkRow(1, 4, 7, move) ||
+        checkRow(2, 5, 8, move) ||
+        checkRow(0, 4, 8, move) ||
+        checkRow(2, 4, 6, move)) {  //winning combinations are across, sideways and diagonally
+        result = true;
+    }
+    return result;
+}
+
+//check row of 3 
+function checkRow(a, b, c, move) {
+    var result = false;
+    if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+        result = true;  //if a, b, c have the same letter, return true
+    }
+    return result;
+}
+
+//get the box id 
+function getBox(number) {
+    document.getElementById("s" + number).innerHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
